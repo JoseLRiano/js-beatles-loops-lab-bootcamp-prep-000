ea5bc032f030b15c0f0782dc91a5b43a67347c17
Jose Riano <joselriano10@gmail.com>
2017-09-27 04:44:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function theBeatlesPlay(musicians, instruments){
 function johnLennonFacts(facts){
   var newFacts = [];
   var count =0;
-  while(facts.length > 0){
+  while(count < facts.length){
     newFacts.push(facts[count]+"!!!");
     count++;
   }
